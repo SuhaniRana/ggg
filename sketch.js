51cca99f9da221a30ad610f6d112b0bf6c57a468
SuhaniRana <70816383+SuhaniRana@users.noreply.github.com>
2020-11-16 21:56:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var block8, block9, block10;
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  block8 = new Block(330,235,30,40); 
+  block9 = new Block(360,235,30,40); 
+  block10 = new Block(390,235,30,40); 
+  block11 = new Block(420,235,30,40); 
+  block12 = new Block(450,235,30,40); 
+
+  block13 = new Block(330,195,30,40); 
+  block14 = new Block(330,235,30,40); 
+  block15 = new Block(330,235,30,40); 
+
+  block16 = new Block(390,155,30,40); 
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+}
